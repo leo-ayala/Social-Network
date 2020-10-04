7ae7d59f1bcac95ac90886f6ddf00339a2ccb3e4
Leo Ayala <leo_a1016@yahoo.com>
2020-10-04 13:38:23 -0400

    added models

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const ReactionsSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // uses ReactionsSchema to validate data for a reply
+        reactions: [ReactionsSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('ReactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const UserRoutes = require('./User-routes');
+const ThoughtsRoutes = require('./Thoughts-routes');
+
+// router.use('/comments', commentRoutes);
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+// router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
